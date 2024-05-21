6ef9eba4fa6d434efb362bde090ce5f49ae9b96c
Sacha Servan-Schreiber <8027773+sachaservan@users.noreply.github.com>
2024-05-21 14:15:25 -0700

    Add missing function to FFT lib

--- libs/fft/include/fft.h
+++ libs/fft/include/fft.h
@@ -31,4 +31,28 @@ void fft_recursive_uint8(
     const size_t num_vars,
     const size_t num_coeffs);
 
+// Compute base^exp without the floating-point precision
+// errors of the built-in pow function.
+static inline size_t ipow(size_t base, size_t exp)
+{
+    if (exp == 1)
+        return base;
+
+    if (exp == 0)
+        return 1;
+
+    size_t result = 1;
+    while (1)
+    {
+        if (exp & 1)
+            result *= base;
+        exp >>= 1;
+        if (!exp)
+            break;
+        base *= base;
+    }
+
+    return result;
+}
+
 #endif
